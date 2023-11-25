663e90f810d5d6075eab34823d153445bc269ccc
pujanhero456 <pujanvasania@gmail.com>
2023-11-25 10:57:27 +0530

    Update cors

--- server.js
+++ server.js
@@ -12,54 +12,53 @@ dotenv.config();
 const app = express();
 const port = 3001;
 
-app.use(express.json())
-app.use(cors({ origin: 'http://localhost:3000/' }));
+app.use(express.json());
 
+// Allow requests from the specified origins, including http://localhost:3000
+const allowedOrigins = ['http://localhost:3000'];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
-
+app.use(cors(corsOptions));
 
 mongoose.connect(`${process.env.MONGODB_URL}`).then(() => console.log("connected to db"))
-.catch(console.error)
+  .catch(console.error);
 
-
-
-app.get("/todos",  async (req, res) => {
-    const todos = await Todo.find();
-    res.json(todos)
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
 });
 
-app.post("/todo/new",  (req,res) => {
+app.post("/todo/new", (req, res) => {
   const todo = new Todo({
     text: req.body.text,
-    priority: req.body.priority
-  })
+    priority: req.body.priority,
+  });
 
-  todo.save();  
+  todo.save();
+  res.json(todo);
+});
 
-  res.json(todo)
-})
-
-app.delete("/todo/delete/:id", async (req,res) => {
-  const result = await Todo.deleteOne({_id: req.params.id});
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.deleteOne({ _id: req.params.id });
   res.json(result);
-})
+});
 
-
-app.patch("/todo/complete/:id", async (req,res) => {
+app.patch("/todo/complete/:id", async (req, res) => {
   const todo = await Todo.findById(req.params.id);
   todo.complete = !todo.complete;
   todo.save();
-
   res.json(todo);
-})
-
-
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-
-
-
